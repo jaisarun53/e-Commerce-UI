f86d98fc22deb5eb8d28dfe2e0a9352a0efd3e88
jaisarun53 <jaisarun53@gmail.com>
2024-05-20 21:06:44 +0545

    register via frontend

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,117 +1,113 @@
-import React from "react";
-import { Formik, validateYupSchema } from "formik";
 import {
+  Box,
   Button,
   FormControl,
   FormHelperText,
+  LinearProgress,
   TextField,
   Typography,
-  InputLabel,
-  Select,
-  MenuItem,
-  OutlinedInput,
-  InputAdornment,
-  IconButton,
-  CircularProgress,
 } from "@mui/material";
-import { Password, Visibility, VisibilityOff } from "@mui/icons-material";
+import { Formik } from "formik";
+import React from "react";
+
 import { useMutation } from "@tanstack/react-query";
-import { loginValidationSchema } from "../ValidationSchema/Login.validation.schema";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import $axios from "../lib/axios/axios.instance";
+import { loginValidationSchema } from "../ValidationSchema/Register.validation.schema";
+
 const LoginForm = () => {
+  const navigate = useNavigate();
+  // const dispatch = useDispatch();
+
   const { isPending, mutate } = useMutation({
     mutationKey: ["login-user"],
     mutationFn: async (values) => {
+      console.log(values);
       return await $axios.post("/user/login", values);
     },
+    onSuccess: (res) => {
+      console.log(res);
+      navigate("/homepage");
+
+      // extract token, role and first name from login response
+      // const accessToken = res?.data?.accessToken;
+      // const firstName = res?.data?.userDetails?.firstName;
+      // const role = res?.data?.userDetails?.role;
+
+      // set these values to local storage
+      // localStorage.setItem("accessToken", accessToken);
+      // localStorage.setItem("firstName", firstName);
+      // localStorage.setItem("role", role);
+    },
+    onError: (error) => {
+      dispatch(openErrorSnackbar(error?.response?.data?.message));
+    },
   });
-  if (isPending) {
-    return <CircularProgress />;
-  }
 
-  const [showPassword, setShowPassword] = React.useState(false);
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
   return (
-    <div>
-      <Formik
-        initialValues={{
-          email: "",
+    <>
+      {isPending && <LinearProgress color="secondary" />}
+      <Box>
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          validationSchema={loginValidationSchema}
+          onSubmit={(values) => {
+            mutate(values);
+          }}
+        >
+          {(formik) => {
+            return (
+              <form
+                onSubmit={formik.handleSubmit}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-around",
+                  padding: "1rem",
+                  gap: "1rem",
+                  width: "400px",
+                  boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+                }}
+              >
+                <Typography variant="h4">Sign in</Typography>
+                <FormControl>
+                  <TextField label="Email" {...formik.getFieldProps("email")} />
+                  {formik.touched.email && formik.errors.email ? (
+                    <FormHelperText error>{formik.errors.email}</FormHelperText>
+                  ) : null}
+                </FormControl>
 
-          password: "",
-        }}
-        validationSchema={loginValidationSchema}
-        onSubmit={(values) => {
-          console.log(values);
-          mutate(values);
-        }}
-      >
-        {(formik) => (
-          <form
-            onSubmit={formik.handleSubmit}
-            style={{
-              padding: "1rem",
-              display: "flex",
-              flexDirection: "column",
-              alignContent: "center",
-              justifyContent: "center",
-              width: "400px",
-              gap: "1rem",
-              boxShadow:
-                "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
-            }}
-          >
-            <Typography variant="h4">Sign in</Typography>
+                <FormControl>
+                  <TextField
+                    label="Password"
+                    {...formik.getFieldProps("password")}
+                  />
+                  {formik.touched.password && formik.errors.password ? (
+                    <FormHelperText error>
+                      {formik.errors.password}
+                    </FormHelperText>
+                  ) : null}
+                </FormControl>
 
-            <FormControl>
-              <TextField
-                label="Email"
-                {...formik.getFieldProps("email")}
-                required
-              />
-              {formik.touched.email && formik.errors.email ? (
-                <FormHelperText error>{formik.errors.email}</FormHelperText>
-              ) : null}
-            </FormControl>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  type="submit"
+                  disabled={isPending}
+                >
+                  Login
+                </Button>
 
-            <FormControl variant="outlined" fullWidth>
-              <InputLabel htmlFor="outlined-adornment-password">
-                Password
-              </InputLabel>
-              <OutlinedInput
-                {...formik.getFieldProps("password")}
-                type={showPassword ? "text" : "password"}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                label="Password"
-              />
-              {formik.touched.password && formik.errors.password ? (
-                <FormHelperText error>{formik.errors.password}</FormHelperText>
-              ) : null}
-            </FormControl>
-
-            {/* {console.log(formik.errors)} */}
-            <Button type="submit" color="secondary" variant="contained">
-              Login
-            </Button>
-            <Link to="/register">New here? Register</Link>
-          </form>
-        )}
-      </Formik>
-    </div>
+                <Link to="/register">New here? Register</Link>
+              </form>
+            );
+          }}
+        </Formik>
+      </Box>
+    </>
   );
 };
 
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -11,14 +11,35 @@ import {
   TextField,
   Typography,
   OutlinedInput,
+  LinearProgress,
 } from "@mui/material";
 import { Formik } from "formik";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { registerValidateSchema } from "../ValidationSchema/Register.validation.schema";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useMutation } from "@tanstack/react-query";
+import $axios from "../lib/axios/axios.instance";
 
 const Register = () => {
+  // hit register api
+  const navigate = useNavigate();
+
+  const { isPending, mutate } = useMutation({
+    mutationKey: ["register-user"],
+    mutationFn: async (values) => {
+      console.log(values);
+      return await $axios.post("/user/register", values);
+    },
+    onSuccess: (res) => {
+      console.log(res);
+      navigate("/login");
+    },
+    onError: (error) => {
+      dispatch(openErrorSnackbar(error?.response?.data?.message));
+    },
+  });
+  // hide password
   const [showPassword, setShowPassword] = React.useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -26,124 +47,138 @@ const Register = () => {
     event.preventDefault();
   };
   return (
-    <Box>
-      {/* <Typography>hello</Typography> */}
-      <Formik
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-          password: "",
-          role: "",
-          gender: "",
-        }}
-        validationSchema={registerValidateSchema}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
-      >
-        {({ handleSubmit, touched, errors, getFieldProps }) => {
-          return (
-            <form
-              onSubmit={handleSubmit}
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                padding: "1rem",
-                gap: "1rem",
-                width: "350px",
-                boxShadow:
-                  "rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px",
-              }}
-            >
-              <Typography variant="h4">Register</Typography>
-              <FormControl>
-                <TextField
-                  label="First name"
-                  {...getFieldProps("firstName")}
-                  required
-                />
-                {touched.firstName && errors.firstName ? (
-                  <FormHelperText error>{errors.firstName}</FormHelperText>
-                ) : null}
-              </FormControl>
+    <>
+      {isPending && <LinearProgress color="secondary" />}
+      <Box>
+        {/* <Typography>hello</Typography> */}
+        <Formik
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            role: "",
+            gender: "",
+          }}
+          validationSchema={registerValidateSchema}
+          onSubmit={(values) => {
+            mutate(values);
+          }}
+        >
+          {({ handleSubmit, touched, errors, getFieldProps }) => {
+            return (
+              <form
+                onSubmit={handleSubmit}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  padding: "1rem",
+                  gap: "1rem",
+                  width: "350px",
+                  boxShadow:
+                    "rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px",
+                }}
+              >
+                <Typography variant="h4">Register</Typography>
+                <FormControl>
+                  <TextField
+                    label="First name"
+                    {...getFieldProps("firstName")}
+                    required
+                  />
+                  {touched.firstName && errors.firstName ? (
+                    <FormHelperText error>{errors.firstName}</FormHelperText>
+                  ) : null}
+                </FormControl>
 
-              <FormControl>
-                <TextField
-                  label="Last name"
-                  {...getFieldProps("lastName")}
-                  required
-                />
-                {touched.lastName && errors.lastName ? (
-                  <FormHelperText error>{errors.lastName}</FormHelperText>
-                ) : null}
-              </FormControl>
+                <FormControl>
+                  <TextField
+                    label="Last name"
+                    {...getFieldProps("lastName")}
+                    required
+                  />
+                  {touched.lastName && errors.lastName ? (
+                    <FormHelperText error>{errors.lastName}</FormHelperText>
+                  ) : null}
+                </FormControl>
 
-              <FormControl>
-                <TextField label="Email" {...getFieldProps("email")} required />
-                {touched.email && errors.email ? (
-                  <FormHelperText error>{errors.email}</FormHelperText>
-                ) : null}
-              </FormControl>
+                <FormControl>
+                  <TextField
+                    label="Email"
+                    {...getFieldProps("email")}
+                    required
+                  />
+                  {touched.email && errors.email ? (
+                    <FormHelperText error>{errors.email}</FormHelperText>
+                  ) : null}
+                </FormControl>
 
-              <FormControl variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-password">
-                  Password
-                </InputLabel>
-                <OutlinedInput
-                  {...getFieldProps("password")}
-                  type={showPassword ? "text" : "password"}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  label="Password"
-                />
-                {touched.password && errors.password ? (
-                  <FormHelperText error>{errors.password}</FormHelperText>
-                ) : null}
-              </FormControl>
+                <FormControl variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Password
+                  </InputLabel>
+                  <OutlinedInput
+                    {...getFieldProps("password")}
+                    type={showPassword ? "text" : "password"}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                  />
+                  {touched.password && errors.password ? (
+                    <FormHelperText error>{errors.password}</FormHelperText>
+                  ) : null}
+                </FormControl>
 
-              <FormControl fullWidth required>
-                <InputLabel>Role</InputLabel>
-                <Select {...getFieldProps("role")} label="Role">
-                  <MenuItem value="buyer">Buyer</MenuItem>
-                  <MenuItem value="seller">Seller</MenuItem>
-                </Select>
-                {touched.role && errors.role ? (
-                  <FormHelperText error>{errors.role}</FormHelperText>
-                ) : null}
-              </FormControl>
+                <FormControl fullWidth required>
+                  <InputLabel>Role</InputLabel>
+                  <Select {...getFieldProps("role")} label="Role">
+                    <MenuItem value="buyer">Buyer</MenuItem>
+                    <MenuItem value="seller">Seller</MenuItem>
+                  </Select>
+                  {touched.role && errors.role ? (
+                    <FormHelperText error>{errors.role}</FormHelperText>
+                  ) : null}
+                </FormControl>
 
-              <FormControl fullWidth required>
-                <InputLabel>Gender</InputLabel>
-                <Select {...getFieldProps("gender")} label="Gender">
-                  <MenuItem value="male">Male</MenuItem>
-                  <MenuItem value="female">Female</MenuItem>
-                  <MenuItem value="preferNotToSay">Prefer Not To Say</MenuItem>
-                </Select>
-                {touched.gender && errors.gender ? (
-                  <FormHelperText error>{errors.gender}</FormHelperText>
-                ) : null}
-              </FormControl>
+                <FormControl fullWidth required>
+                  <InputLabel>Gender</InputLabel>
+                  <Select {...getFieldProps("gender")} label="Gender">
+                    <MenuItem value="male">Male</MenuItem>
+                    <MenuItem value="female">Female</MenuItem>
+                    <MenuItem value="preferNotToSay">
+                      Prefer Not To Say
+                    </MenuItem>
+                  </Select>
+                  {touched.gender && errors.gender ? (
+                    <FormHelperText error>{errors.gender}</FormHelperText>
+                  ) : null}
+                </FormControl>
 
-              <Button color="secondary" variant="contained" type="submit">
-                Sign up
-              </Button>
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  type="submit"
+                  disabled={isPending}
+                >
+                  Sign up
+                </Button>
 
-              <Link to="/login">Already Registered? Login</Link>
-            </form>
-          );
-        }}
-      </Formik>
-    </Box>
+                <Link to="/login">Already Registered? Login</Link>
+              </form>
+            );
+          }}
+        </Formik>
+      </Box>
+    </>
   );
 };
 export default Register;
